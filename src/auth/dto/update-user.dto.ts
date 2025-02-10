1ce5ae7f724ef82dadc18f846c43027a9e9bac40
.notCoder <80260216+SatvikSv@users.noreply.github.com>
2025-02-10 16:46:38 +0530

    fix/updateuser-dto

--- src/auth/dto/update-user.dto.ts
+++ src/auth/dto/update-user.dto.ts
@@ -4,11 +4,4 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiProperty({
-    example: '12345',
-    description: 'plan id',
-  })
-  @IsString()
-  @IsOptional()
-  plan_id?: string;
 }
