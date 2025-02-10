25c35024b512ea1d93fe02a70821636b3daad3e0
.notCoder <80260216+SatvikSv@users.noreply.github.com>
2025-02-10 15:42:04 +0530

    feat/rabc

--- src/packages/authorization/permission-key.enum.ts
+++ src/packages/authorization/permission-key.enum.ts
@@ -1,3 +1,13 @@
-export const enum PermissionKey{
-    
+export const enum PermissionKey {
+    // user
+    CreateUser = '141',
+    GetUser = '142',
+    EditUser = '143',
+    DeleteUser = '144',
+
+    // role
+    CreateRole = '161',
+    GetRole = '162',
+    EditRole = '163',
+    DeleteRole = '164',
 }
