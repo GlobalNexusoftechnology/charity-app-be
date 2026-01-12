33732584f2e84a180eca51afaf4effc9b9fd689e
faizahmedchoudhary <ahmedfaiz868@gmail.com>
2026-01-12 20:19:06 +0530

    changes

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/user.entity';
+import { Donations } from 'src/payment/entity/donation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users, Donations])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService ],
   exports: [UserService],
 })
 export class UserModule {}
